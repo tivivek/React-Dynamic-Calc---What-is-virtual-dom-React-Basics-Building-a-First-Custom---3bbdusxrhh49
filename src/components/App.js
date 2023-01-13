0338ec71628f4d03562a77e5ef1402a9d81eabe4
Vivek Tiwari <79350756+tivivek@users.noreply.github.com>
2023-01-14 00:01:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [firstNum, setFirstNum] = useState(0);
+  const [secondNum, setSecondNum] = useState(0);
+  
+  const handleChange1 =(event)=> {setFirstNum(event.target.value)}
+  const handleChange2 =(event)=> {setSecondNum(event.target.value)}
 
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id='input1' onChange={handleChange1}/>
        +
-      <input id='input2' onChange={}/> 
+      <input id='input2' onChange={handleChange2}/> 
       
-      <p id='result'></p>
+      <p id='result'>{parseInt(firstNum)+parseInt(secondNum)}</p>
     </div>
   )
 }
